eee3f4bba232c966f9979afb46bb6afc2245b0d2
Noor Ncho <nrandin@gmail.com>
2021-09-09 14:52:35 +0100

    implemented article render

--- src/components/ArticleTile/ArticleTile.jsx
+++ src/components/ArticleTile/ArticleTile.jsx
@@ -2,20 +2,44 @@ import React from 'react';
 import "./ArticleTile.scss";
 
 const ArticleTile = (props) => {
-    const {thumbnail, title,  author, publishDate, readTime, description} = props;
+    const {articlesArr} = props;
     
-    return (
-        <div className="article-tile">
-            <img className="article-thumbnail" src={thumbnail} alt="" />
-            <div className="article-summary">
-                <p className="article-title">{title}</p>
-                <p className="article-description">{description}</p>
-            </div>
-            <div className="article-info">
-                <p className="article-author">{author}</p>
-                <p className="article-date">{publishDate} ~ {readTime}min read</p>
-            </div>            
-        </div>
+    const artcleListJSX = articlesArr.map((article) => {
+        let articleTileStyle = "article-tile";
+        
+        if(article.isFeatured){
+            articleTileStyle += " featured";
+        }
+        if(article.isLatest){
+            articleTileStyle += " latest";
+        }
+        if(article.isPopular){
+            articleTileStyle += " polular";
+        }
+
+        return(            
+            <>
+                <div className={articleTileStyle}>
+                    <img key={article.idArticle} src={article.strThumbnail} alt="" />
+                    <div className="article-summary">
+                        <p className="article-title">{article.strArticleTile}</p>
+                        <p className="article-description">{article.strDescription}</p>
+                    </div>
+                    <div className="article-info">
+                        <p className="article-author">{article.strAuthor}</p>
+                        <p className="article-date">{article.strPublishDate} ~ {article.strReadTime}min read</p>
+                    </div>
+                </div>
+            </>
+        )
+        
+    });
+    
+
+    return (        
+        <>
+            {artcleListJSX}
+        </>
     )
 }
 
---
+++ src/data/articles.js
@@ -0,0 +1,115 @@
+const articles = [
+    {
+        strAuthor: "Lorem ipsum.",
+        strArticleTitle: "Lorem ipsum dolor sit amet.",
+        strPublishDate: "3 Sept",
+        strReadTime: "3",
+        strThumbnail: "https://placebear.com/200/200.jpg",
+        strDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis justo vel felis porta, id.",
+        isFeatured: true,
+        isLatest: false,
+        isPopular: false
+    },{
+        idArticle: "01",
+        strAuthor: "Lorem ipsum.",
+        strArticleTitle: "Lorem ipsum dolor sit amet.",
+        strPublishDate: "3 Sept",
+        strReadTime: "3",
+        strThumbnail: "https://placebear.com/200/200.jpg",
+        strDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis justo vel felis porta, id.",
+        isFeatured: false,
+        isLatest: true,
+        isPopular: false
+    },{
+        idArticle: "02",
+        strAuthor: "Lorem ipsum.",
+        strArticleTitle: "Lorem ipsum dolor sit amet.",
+        strPublishDate: "3 Sept",
+        strReadTime: "3",
+        strThumbnail: "https://placebear.com/200/200",
+        strDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis justo vel felis porta, id.",
+        isFeatured: false,
+        isLatest: true,
+        isPopular: false
+    },{
+        idArticle: "03",
+        strAuthor: "Lorem ipsum.",
+        strArticleTitle: "Lorem ipsum dolor sit amet.",
+        strPublishDate: "3 Sept",
+        strReadTime: "3",
+        strThumbnail: "https://placebear.com/200/200",
+        strDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis justo vel felis porta, id.",
+        isFeatured: false,
+        isLatest: true,
+        isPopular: false
+    },{
+        idArticle: "04",
+        strAuthor: "Lorem ipsum.",
+        strArticleTitle: "Lorem ipsum dolor sit amet.",
+        strPublishDate: "3 Sept",
+        strReadTime: "3",
+        strThumbnail: "https://placebear.com/200/200",
+        strDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis justo vel felis porta, id.",
+        isFeatured: false,
+        isLatest: true,
+        isPopular: false
+    },{
+        idArticle: "05",
+        strAuthor: "Lorem ipsum.",
+        strArticleTitle: "Lorem ipsum dolor sit amet.",
+        strPublishDate: "3 Sept",
+        strReadTime: "3",
+        strThumbnail: "https://placebear.com/200/200.jpg",
+        strDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis justo vel felis porta, id.",
+        isFeatured: false,
+        isLatest: true,
+        isPopular: false
+    },{
+        idArticle: "06",
+        strAuthor: "Lorem ipsum.",
+        strArticleTitle: "Lorem ipsum dolor sit amet.",
+        strPublishDate: "3 Sept",
+        strReadTime: "3",
+        strThumbnail: "https://placebear.com/200/200",
+        strDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis justo vel felis porta, id.",
+        isFeatured: false,
+        isLatest: false,
+        isPopular: true
+    },{
+        idArticle: "07",
+        strAuthor: "Lorem ipsum.",
+        strArticleTitle: "Lorem ipsum dolor sit amet.",
+        strPublishDate: "3 Sept",
+        strReadTime: "3",
+        strThumbnail: "https://placebear.com/200/200",
+        strDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis justo vel felis porta, id.",
+        isFeatured: false,
+        isLatest: false,
+        isPopular: true
+    },{
+        idArticle: "08",
+        strAuthor: "Lorem ipsum.",
+        strArticleTitle: "Lorem ipsum dolor sit amet.",
+        strPublishDate: "3 Sept",
+        strReadTime: "3",
+        strThumbnail: "https://placebear.com/200/200",
+        strDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis justo vel felis porta, id.",
+        isFeatured: false,
+        isLatest: false,
+        isPopular: true
+    },{
+        idArticle: "09",
+        strAuthor: "Lorem ipsum.",
+        strArticleTitle: "Lorem ipsum dolor sit amet.",
+        strPublishDate: "3 Sept",
+        strReadTime: "3",
+        strThumbnail: "https://placebear.com/200/200",
+        strDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis justo vel felis porta, id.",
+        isFeatured: false,
+        isLatest: false,
+        isPopular: true
+    }
+];
+
+
+export default articles;
