06aa4c5b7f19e6141da1f5cabec72ec38c12c051
Noor Ncho <nrandin@gmail.com>
2021-09-09 14:52:56 +0100

    added featured and latest articles

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,28 @@
 
 import React from 'react'
 import "./App.scss";
+
 import Nav from './components/Nav/Nav';
+import ArticleTile from './components/ArticleTile/ArticleTile';
+
+import articles from "./data/articles.js";
 
 const App = () => {
+  const featuredArticle = articles.filter(article => article.isFeatured);
+  const latestArticles = articles.filter(article => article.isLatest);
+  console.log(latestArticles);
+  console.log(featuredArticle);
+
   return (
-      <div>
+      <>
         <Nav />
-      </div>
-      
+        <section className="featured-article">
+          <ArticleTile articlesArr={featuredArticle}/>
+        </section>
+        <section className="latest-articles">
+          <ArticleTile articlesArr={latestArticles}/>
+        </section>
+      </>      
     )
 }
 
