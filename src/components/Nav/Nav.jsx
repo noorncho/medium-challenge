96d63604a762449f1ce86b76389474f1d32e32f8
Noor Ncho <nrandin@gmail.com>
2021-09-13 16:38:14 +0100

    Updated Nav components

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,13 +6,13 @@ const Nav = () => {
     return (
         <nav>
             <div className="nav-title">
-                <h1>Meidum</h1> 
+                <h1>Medium</h1> 
                 <h2>| Programming</h2>
             </div>
             <div className="nav-items">
-                <a href="#1">Become a Memeber</a>
+                <a href="#1">Become a Member</a>
                 <a href="#2">Sign in</a>
-                <Button buttonText="Get started" isPrimary />
+                <Button buttonText="Get started" isPrimary/>
             </div>
         </nav>
     )
