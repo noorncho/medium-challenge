2157e613f2c79ae1b03901b0a4566bfe018362ed
Noor Ncho <nrandin@gmail.com>
2021-09-13 16:50:30 +0100

    updated button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import "./Button.scss";
 
 const Button = (props) => {
+    //const {buttonText} = props
     const {buttonText, isPrimary} = props;
     const buttonStyle = isPrimary ? "button primary" : "button secondary";
 
     return (
         <button className={buttonStyle}>{buttonText}</button>
+        //<button>{buttonText}</button>
     )
 }
 
