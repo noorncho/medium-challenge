24eb128f0b2d1607285cfa68df616fbc2bf3830d
Noor Ncho <nrandin@gmail.com>
2021-09-13 16:38:40 +0100

    created Featured writer tile

--- src/components/FeaturedWriterTile/FeaturedWriterTile.jsx
+++ src/components/FeaturedWriterTile/FeaturedWriterTile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./FeaturedWriterTile.scss";
 import articles from '../../data/articles';
 import ArticleTile from '../ArticleTile/ArticleTile';
 
@@ -7,13 +8,19 @@ const FeaturedWriterTile = (props) => {
     const featuredWriterArticles = articles.filter(article => article.strAuthor.match(featuredWriter)).slice(0, 3);
 
     return (
-        <>
+        <div className="featured-tile">
+            <div>
+                <img className="featured-tile__pfp" src="https://placebear.com/200/200" alt=""/>
+                <button className="featured-tile__button">Follow</button>
+            </div>
             <div className="writer-profile">
                 <h4>{featuredWriter}</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit quia ea incidunt quibusdam consequuntur et consequatur natus.</p>
+                <p className="writer-profile__bio">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit quia ea incidunt quibusdam consequuntur et consequatur natus.</p>
             </div>
-            <ArticleTile articlesArr={featuredWriterArticles} />
-        </>
+            <div className="writer-profile__articles">
+                <ArticleTile articlesArr={featuredWriterArticles} />
+            </div>
+        </div>
     )
 }
 
